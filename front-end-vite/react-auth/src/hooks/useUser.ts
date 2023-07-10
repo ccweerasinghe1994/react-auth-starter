f5a857b6a67723f1c2b683253f7259c262f45c2d
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-10 13:55:04 +0530

    Update useUser.ts

--- front-end-vite/react-auth/src/hooks/useUser.ts
+++ front-end-vite/react-auth/src/hooks/useUser.ts
@@ -1,11 +1,22 @@
 import { useToken } from './useToken';
 import { useState, useEffect } from 'react';
+
+export type User = {
+	id: string;
+	email: string;
+	info: {
+		hairColor: string;
+		favoriteFood: string;
+		bio: string;
+	};
+};
+
 export const useUser = () => {
 	const { token } = useToken();
 
 	const getPayloadFromToken = (token: string) => {
 		const encodedPayload = token.split('.')[1];
-		return JSON.parse(atob(encodedPayload)) as object;
+		return JSON.parse(atob(encodedPayload)) as User;
 	};
 
 	const [user, setUser] = useState(() => {
