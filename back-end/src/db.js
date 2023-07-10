0788624c7f045cb03a01232d706f82037e277c99
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-10 13:54:47 +0530

    Update db.js

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -2,7 +2,6 @@ import { MongoClient } from 'mongodb';
 const uri = "mongodb://0.0.0.0:27017/";
 const client = new MongoClient(uri);
 // let client;
-const dbName = 'myProject';
 export const initializeDbConnection = async () => {
     await client.connect();
     console.log('Connected successfully to server');
